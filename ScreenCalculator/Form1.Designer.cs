812b8d35d6aa435f15d4a488f1f780b84bbf4117
jwstes <jwstes@gmail.com>
2018-02-25 12:05:36 +0800

    Add files via upload

---
+++ ScreenCalculator/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace ScreenCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.currentmonitorsize = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comparemonitorsize = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btncompare = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // currentmonitorsize
+            // 
+            this.currentmonitorsize.BackColor = System.Drawing.SystemColors.Window;
+            this.currentmonitorsize.Font = new System.Drawing.Font("Work Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.currentmonitorsize.FormattingEnabled = true;
+            this.currentmonitorsize.Items.AddRange(new object[] {
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "27"});
+            this.currentmonitorsize.Location = new System.Drawing.Point(209, 66);
+            this.currentmonitorsize.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.currentmonitorsize.Name = "currentmonitorsize";
+            this.currentmonitorsize.Size = new System.Drawing.Size(76, 31);
+            this.currentmonitorsize.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Work Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 69);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Your Monitor\'s Size";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Work Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(12, 111);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Screen To Compare";
+            // 
+            // comparemonitorsize
+            // 
+            this.comparemonitorsize.Font = new System.Drawing.Font("Work Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comparemonitorsize.FormattingEnabled = true;
+            this.comparemonitorsize.Items.AddRange(new object[] {
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "27"});
+            this.comparemonitorsize.Location = new System.Drawing.Point(209, 108);
+            this.comparemonitorsize.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.comparemonitorsize.Name = "comparemonitorsize";
+            this.comparemonitorsize.Size = new System.Drawing.Size(75, 31);
+            this.comparemonitorsize.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Work Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(12, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ratio : 16:9";
+            // 
+            // btncompare
+            // 
+            this.btncompare.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btncompare.Font = new System.Drawing.Font("Work Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncompare.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btncompare.Location = new System.Drawing.Point(16, 187);
+            this.btncompare.Name = "btncompare";
+            this.btncompare.Size = new System.Drawing.Size(269, 33);
+            this.btncompare.TabIndex = 5;
+            this.btncompare.Text = "Compare";
+            this.btncompare.UseVisualStyleBackColor = true;
+            this.btncompare.Click += new System.EventHandler(this.btncompare_Click);
+            // 
+            // result
+            // 
+            this.result.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.result.Font = new System.Drawing.Font("Work Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result.ForeColor = System.Drawing.Color.White;
+            this.result.Location = new System.Drawing.Point(295, 12);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(268, 247);
+            this.result.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Work Sans", 14.25F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(16, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(269, 33);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Show Formular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(443, 240);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Made by - jwstes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(575, 269);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.btncompare);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comparemonitorsize);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.currentmonitorsize);
+            this.Font = new System.Drawing.Font("Work Sans", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ScreenCalculator - jwstes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox currentmonitorsize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comparemonitorsize;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btncompare;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ScreenCalculator/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static double width;
+
+        static double height;
+
+        public void btncompare_Click(object sender, EventArgs e)
+        {
+            double currentms = Convert.ToDouble(currentmonitorsize.SelectedItem);
+            double comparems = Convert.ToDouble(comparemonitorsize.SelectedItem);
+
+            double currentd = Convert.ToDouble(currentmonitorsize.SelectedItem);
+            double compared = Convert.ToDouble(comparemonitorsize.SelectedItem);
+
+            double cssdcw;
+            double cssdch;
+
+            double cossdcw;
+            double cossdch;
+
+            screensizedata(currentd);
+            cssdcw = width;
+            cssdch = height;
+
+            screensizedata(compared);
+            cossdcw = width;
+            cossdch = height;
+
+            if (currentms > comparems)
+            {
+                double differential;
+                differential = getdifferential(comparems, currentms);
+
+                result.Text =
+                    $"Current Monitor's Dimensions : " + "\n\n" +
+                    $"W: {Convert.ToString((cssdcw))}" + " H: " + $"{ Convert.ToString((cssdch))} "
+                    + "\n\n" + $"Compare Monitor's Dimensions :" + "\n\n" +
+                    $"W: {Convert.ToString(cossdcw)}" + " H: " + $"{ Convert.ToString(cossdch)}"
+                    + "\n\n" + $"Difference :" + "\n\n" +
+                    $"W: {Convert.ToString((cssdcw - cossdcw))}" + $" H: {Convert.ToString((cssdch - cossdch))}"
+                    + $" D: {Convert.ToString(differential)}";
+            }
+            else
+            {
+                double differential;
+                differential = getdifferential(currentms, comparems);
+
+                result.Text =
+                    $"Current Monitor's Dimensions : " + "\n\n" +
+                    $"W: {Convert.ToString((cssdcw))}" + " H: " + $"{ Convert.ToString((cssdch))} "
+                    + "\n\n" + $"Compare Monitor's Dimensions :" + "\n\n" +
+                    $"W: {Convert.ToString(cossdcw)}" + " H: " + $"{ Convert.ToString(cossdch)}"
+                    + "\n\n" + $"Difference :" + "\n\n" +
+                    $"W: {Convert.ToString((cossdcw - cssdcw))}" + $" H: {Convert.ToString((cossdch - cssdch))}" + $" D: {Convert.ToString(differential)}";
+            }
+
+
+        }
+
+        public double getdifferential(double currentms, double comparems)
+        {
+            double s1ans;
+            s1ans = comparems / currentms;
+            currentms = s1ans;
+            return currentms;
+        }
+
+
+        public static Tuple<double?, double?, double?> screensizedata(double data)
+        {
+            switch (data)
+            {
+                case 20:
+                    width = 44.28;
+                    height = 24.91;
+                    break;
+
+                case 21:
+                    width = 47.60;
+                    height = 26.77;
+                    break;
+
+                case 22:
+                    width = 48.70;
+                    height = 27.40;
+                    break;
+
+                case 23:
+                    width = 50.92;
+                    height = 28.64;
+                    break;
+
+                case 24:
+                    width = 53.13;
+                    height = 29.89;
+                    break;
+
+                case 27:
+                    width = 59.77;
+                    height = 33.62;
+                    break;
+
+            }
+
+            return new Tuple<double?, double?, double?>(width, height, data);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            currentmonitorsize.SelectedIndex = 0;
+            comparemonitorsize.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            result.Text = @"Example:" + "\n" + @"27 / 20 = 1.35 (Your Differential)" + "\n" +
+             "\n" + "1.35 * Your current screen's height/width to find your new" + "\n"
+            + "screen's height/width.";
+        }
+    }
+}
